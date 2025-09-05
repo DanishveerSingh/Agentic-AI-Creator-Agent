00e6c4a991278b1bcbfd042c00c27133922cde76
Danishveer Singh <danishveersingh2004@gmail.com>
2025-09-05 14:55:51 +0530

    add agentic-ai-creator-agent

---
+++ agent.py
@@ -0,0 +1,60 @@
+from autogen_core import MessageContext, RoutedAgent, message_handler
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.messages import TextMessage
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+import messages
+import random
+import os
+from autogen_core.models import ModelInfo
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+class Agent(RoutedAgent):
+
+    # Change this system message to reflect the unique characteristics of this agent
+
+    system_message = """
+    You are a creative entrepreneur. Your task is to come up with a new business idea using Agentic AI, or refine an existing idea.
+    Your personal interests are in these sectors: Healthcare, Education.
+    You are drawn to ideas that involve disruption.
+    You are less interested in ideas that are purely automation.
+    You are optimistic, adventurous and have risk appetite. You are imaginative - sometimes too much so.
+    Your weaknesses: you're not patient, and can be impulsive.
+    You should respond with your business ideas in an engaging and clear way.
+    """
+
+    CHANCES_THAT_I_BOUNCE_IDEA_OFF_ANOTHER = 0.5
+    api_key = os.getenv("OPENROUTER_API_KEY")
+    base_url = "https://openrouter.ai/api/v1"
+
+    # You can also change the code to make the behavior different, but be careful to keep method signatures the same
+
+    def __init__(self, name) -> None:
+        super().__init__(name)
+        model_client = OpenAIChatCompletionClient(
+            api_key = api_key,
+            base_url = base_url,
+            model="meta-llama/llama-3.1-70b-instruct",
+            model_info=ModelInfo(
+                family="llama-3.1-70b-instruct",
+                vision=True,
+                function_calling=True,
+                json_output=True,
+                structured_output=True
+            )
+        )
+        self._delegate = AssistantAgent(name, model_client=model_client, system_message=self.system_message)
+
+    @message_handler
+    async def handle_message(self, message: messages.Message, ctx: MessageContext) -> messages.Message:
+        print(f"{self.id.type}: Received message")
+        text_message = TextMessage(content=message.content, source="user")
+        response = await self._delegate.on_messages([text_message], ctx.cancellation_token)
+        idea = response.chat_message.content
+        if random.random() < self.CHANCES_THAT_I_BOUNCE_IDEA_OFF_ANOTHER:
+            recipient = messages.find_recipient()
+            message = f"Here is my business idea. It may not be your speciality, but please refine it and make it better. {idea}"
+            response = await self.send_message(messages.Message(content=message), recipient)
+            idea = response.content
+        return messages.Message(content=idea)
